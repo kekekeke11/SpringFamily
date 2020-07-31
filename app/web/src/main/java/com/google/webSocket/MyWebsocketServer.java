eb8fa25f732d99ad2c66ebe22064ea9d667717b2
白云苍狗の <1357760345@qq.com>
2020-07-31 14:28:31 +0800

    删除多余引入

--- app/web/src/main/java/com/google/webSocket/MyWebsocketServer.java
+++ app/web/src/main/java/com/google/webSocket/MyWebsocketServer.java
@@ -2,8 +2,6 @@ package com.google.webSocket;
 
 import com.google.config.MyApplicationContextAware;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
